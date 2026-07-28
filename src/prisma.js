57ebe9d319a69a839e424d704d020740197ed01c
Vitoco <vitoco@192.168.1.29>
2026-07-27 22:05:17 -0400

    fix: explicit NODE_TLS_REJECT_UNAUTHORIZED in prisma init

--- src/prisma.js
+++ src/prisma.js
@@ -2,8 +2,12 @@ import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import pg from "pg";
 
+const url = process.env.DATABASE_URL || "";
+// Need to disable SSL verification for Railway's self-signed cert
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
+  connectionString: url,
   ssl: { rejectUnauthorized: false },
 });
 const adapter = new PrismaPg(pool);
